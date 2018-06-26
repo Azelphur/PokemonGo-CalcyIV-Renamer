5583b4401f5221a0fd10d7f5a26b59281c96b96d
Umut Kaya Ilkiz <umutkaya2000@gmail.com>
2018-06-25 19:43:21 -0700

    Bind hardcoded values to arguments
    Some arguments were already set but not used in the main loop, and some values were just hardcoded into tap arguments. Added new arguments to cover missing potentially customizable values and used argument values in code.

--- ivcheck.py
+++ ivcheck.py
@@ -22,10 +22,22 @@ parser.add_argument('--sleep_short', type=float, default=0.7,
                     help="Sleep duration for shorter pauses.")
 parser.add_argument('--sleep_long', type=float, default=1.5,
                     help="Sleep duration for longer pauses.")
+parser.add_argument('--name_line_x', type=float, default=50.74,
+                    help="X coordinate (in %) of name edit button position.")
+parser.add_argument('--name_line_y', type=float, default=47.97,
+                    help="Y coordinate (in %) of name edit button position.")
 parser.add_argument('--ok_button_x', type=float, default=86.46,
                     help="X coordinate (in %) of OK button position for keyboard input.")
 parser.add_argument('--ok_button_y', type=float, default=57.08,
                     help="Y coordinate (in %) of OK button position for keyboard input.")
+parser.add_argument('--save_button_x', type=float, default=51.48,
+                    help="X coordinate (in %) of OK button position to name change dialog.")
+parser.add_argument('--save_button_y', type=float, default=55.47,
+                    help="Y coordinate (in %) of OK button position to name change dialog.")
+parser.add_argument('--edit_box_x', type=float, default=92.59,
+                    help="X coordinate (in %) of keyboard input field to open keyboard (used with --nopaste).")
+parser.add_argument('--edit_box_y', type=float, default=88.54,
+                    help="Y coordinate (in %) of keyboard input field to open keyboard (used with --nopaste).")
 parser.add_argument('--edit_line_x', type=float, default=6.67,
                     help="X coordinate (in %) of keyboard input field to open edit dialog for pasting (used with --nopaste).")
 parser.add_argument('--edit_line_y', type=float, default=57.29,
@@ -34,12 +46,16 @@ parser.add_argument('--paste_button_x', type=float, default=11.38,
                     help="X coordinate (in %) of paste button position (used with --nopaste).")
 parser.add_argument('--paste_button_y', type=float, default=64.53,
                     help="Y coordinate (in %) of paste button position (used with --nopaste).")
+parser.add_argument('--calcy_button_x', type=float, default=7.40,
+                    help="X coordinate (in %) of calcyIV button.")
+parser.add_argument('--calcy_button_y', type=float, default=46.87,
+                    help="Y coordinate (in %) of calcyIV button.")
 args = parser.parse_args()
 
 p = pokemonlib.PokemonGo(args.device_id)
 n = 0
 while args.stop_after is None or n < args.stop_after:
-    p.tap(7.40, 46.87, args.sleep_long) # Calcy IV
+    p.tap(args.calcy_button_x, args.calcy_button_y, args.sleep_long) # Calcy IV
     try:
         p.check_calcy_iv()
         skip_count = 0
@@ -52,7 +68,7 @@ while args.stop_after is None or n < args.stop_after:
             input("CalcyIVError, Press enter to continue")
         skip_count = skip_count + 1
         if skip_count > args.max_retries and args.max_retries != 0:
-            print("CalcyIVError 5 times in a row, skipping to next pokemon")
+            print("CalcyIVError " + str(args.max_retries) + " times in a row, skipping to next pokemon")
             n = n + 1
             p.swipe(97.22, 70.31, 4.62, 70.31, args.sleep_short)
             skip_count = 0
@@ -60,15 +76,15 @@ while args.stop_after is None or n < args.stop_after:
 
     if not args.no_rename:
         p.tap(54.91, 69.53, 0) # Dismiss Calcy IV
-        p.tap(50.74, 47.97, args.sleep_short) # Rename
+        p.tap(args.name_line_x, args.name_line_y, args.sleep_short) # Rename
         if args.nopaste:
-            p.tap(92.59, 88.54, args.sleep_short) # Press in the edit box
+            p.tap(args.edit_box_x, args.edit_box_y, args.sleep_short) # Press in the edit box
             p.swipe(args.edit_line_x, args.edit_line_y, args.edit_line_x, args.edit_line_y, args.sleep_short, 600) # Use swipe to simulate a long press to bring up copy/paste dialog
             #p.tap(24.63, 50.42, args.sleep_short) # Press paste
             p.tap(args.paste_button_x, args.paste_button_y, args.sleep_short) # Press paste
         else:
             p.key(279, args.sleep_short) # Paste into rename
-        p.tap(86.48, 57.08, args.sleep_short) # Press OK on edit line
-        p.tap(51.48, 55.47, args.sleep_long) # Press OK on Pokemon go rename dialog
+        p.tap(args.ok_button_x, args.ok_button_y, args.sleep_short) # Press OK on edit line
+        p.tap(args.save_button_x, args.save_button_y, args.sleep_long) # Press OK on Pokemon go rename dialog
     n = n + 1
     p.swipe(97.22, 70.31, 4.63, 70.31, args.sleep_short) # Swipe to next pokemon
