7f71a16f1ca3f6d2f3a452b1f241b09abd04eace
Alfie "Azelphur" Day <support@azelphur.com>
2018-09-18 10:12:42 +0100

    Rewrite using calcy IVs logcat messages. Requires Python3.5+

--- ivcheck.py
+++ ivcheck.py
@@ -1,115 +1,93 @@
-import pokemonlib
+from pokemonlib import PokemonGo
+import yaml
+import asyncio
+import re
 import argparse
-import subprocess
 
-skip_count = 0
+RE_CALCY_IV = re.compile(r"^MainService: Received values: Id: \d+ \((?P<name>.+)\), Nr: (?P<id>\d+), CP: (?P<cp>\d+), Max HP: (?P<max_hp>\d+), Dust cost: (?P<dust_cost>\d+), Level: (?P<level>[0-9\.]+), FastMove (?P<fast_move>.+), SpecialMove (?P<special_move>.+), Gender (?P<gender>\d)$")
+RE_RED_BAR = re.compile(r"^av      : Screenshot #\d has red error box at the top of the screen$")
+RE_FINISHED = re.compile(r".+\s+: calculateScanOutputData finished after \d+ms$")
+RE_SCAN_INVALID = re.compile(r".+\s+: Scan invalid$")
 
-parser = argparse.ArgumentParser(description='Pokemon go renamer')
-parser.add_argument('--device_id', type=str, default=None,
-                    help="Optional, if not specified the phone is automatically detected. Useful only if you have multiple phones connected. Use adb devices to get a list of ids.")
-parser.add_argument('--adb_path', type=str, default="adb",
-                    help="If adb isn't on your PATH, use this option to specify the location of adb.")
-parser.add_argument('--nopaste', action='store_const', const=True, default=False,
-                    help="Use this switch if your device doesn't support the paste key, for example if you're using a Samsung.")
-parser.add_argument('--no_rename', action='store_const', const=True, default=False,
-                    help="Don't rename, useful for just loading every pokemon into calcy IV history for CSV export.")
-parser.add_argument('--wait_after_error', action='store_const', const=True, default=False,
-                    help="Upon calcy IV error, wait for user input.")
-parser.add_argument('--max_retries', type=int, default=5,
-                    help="Maximum retries, set to 0 for unlimited.")
-parser.add_argument('--stop_after', type=int, default=None,
-                    help="Stop after this many pokemon.")
-parser.add_argument('--sleep_short', type=float, default=0.7,
-                    help="Sleep duration for shorter pauses.")
-parser.add_argument('--sleep_long', type=float, default=1.5,
-                    help="Sleep duration for longer pauses.")
-parser.add_argument('--name_line_x', type=float, default=50.74,
-                    help="X coordinate (in %) of name edit button position.")
-parser.add_argument('--name_line_y', type=float, default=47.97,
-                    help="Y coordinate (in %) of name edit button position.")
-parser.add_argument('--ok_button_x', type=float, default=86.46,
-                    help="X coordinate (in %) of OK button position for keyboard input.")
-parser.add_argument('--ok_button_y', type=float, default=57.08,
-                    help="Y coordinate (in %) of OK button position for keyboard input.")
-parser.add_argument('--save_button_x', type=float, default=51.48,
-                    help="X coordinate (in %) of OK button position to name change dialog.")
-parser.add_argument('--save_button_y', type=float, default=55.47,
-                    help="Y coordinate (in %) of OK button position to name change dialog.")
-parser.add_argument('--edit_box_x', type=float, default=92.59,
-                    help="X coordinate (in %) of keyboard input field to open keyboard (used with --nopaste).")
-parser.add_argument('--edit_box_y', type=float, default=88.54,
-                    help="Y coordinate (in %) of keyboard input field to open keyboard (used with --nopaste).")
-parser.add_argument('--edit_line_x', type=float, default=6.67,
-                    help="X coordinate (in %) of keyboard input field to open edit dialog for pasting (used with --nopaste).")
-parser.add_argument('--edit_line_y', type=float, default=57.29,
-                    help="Y coordinate (in %) of keyboard input field to open edit dialog for pasting (used with --nopaste).")
-parser.add_argument('--paste_button_x', type=float, default=11.38,
-                    help="X coordinate (in %) of paste button position (used with --nopaste).")
-parser.add_argument('--paste_button_y', type=float, default=64.53,
-                    help="Y coordinate (in %) of paste button position (used with --nopaste).")
-parser.add_argument('--calcy_button_x', type=float, default=7.40,
-                    help="X coordinate (in %) of calcyIV button.")
-parser.add_argument('--calcy_button_y', type=float, default=46.87,
-                    help="Y coordinate (in %) of calcyIV button.")
-parser.add_argument('--use_intents', type=bool, default=True,
-                    help="Use intents to communicate with calcyIV.")
-args = parser.parse_args()
 
-p = pokemonlib.PokemonGo(args.device_id)
-n = 0
-if args.use_intents:
-    p.send_intent("tesmath.calcy.ACTION_HIDE_BUTTON", "tesmath.calcy/.IntentReceiver", 0)
-p.start_logcat()
-p.read_logcat()
+class CalcyIVError(Exception):
+    pass
 
-def check_calcy_logcat(p):
-    while True:
-        lines = p.read_logcat()
-        for line in lines:
-            if line.endswith(b"has red error box at the top of the screen"):
-                raise pokemonlib.RedBarError
-            if b"MainService: Received values: Id: " in line:
-                if b"-1" in line:
-                    raise pokemonlib.CalcyIVError
+
+class RedBarError(Exception):
+    pass
+
+
+class Main:
+    def __init__(self, args):
+        with open(args.config, "r") as f:
+            self.config = yaml.load(f)
+        self.args = args
+
+    async def tap(self, location):
+        await self.p.tap(*self.config['locations'][location])
+        if location in self.config['waits']:
+            await asyncio.sleep(self.config['waits'][location])
+
+    async def start(self):
+        self.p = PokemonGo()
+        await self.p.start_logcat()
+        num_errors = 0
+        while True:
+            try:
+                values = await self.check_pokemon()
+                num_errors = 0
+            except RedBarError:
+                continue
+            except CalcyIVError:
+                num_errors += 1
+                if num_errors > args.max_retries:
+                    self.tap('next')
+                    num_errors = 0
+                continue
+
+            await self.tap('dismiss_calcy')
+            await self.tap('rename')
+            await self.p.key(279) # Paste into rename
+            await self.tap('keyboard_ok')
+            await self.tap('rename_ok')
+            await self.tap('next')
+
+    async def check_pokemon(self):
+        await self.p.send_intent("tesmath.calcy.ACTION_ANALYZE_SCREEN", "tesmath.calcy/.IntentReceiver")
+        red_bar = False
+        values = None
+        while True:
+            line = await self.p.read_logcat()
+            line = line.decode('utf-8').strip()
+            line = line[33:] # Strip off date and pid
+
+            match = RE_CALCY_IV.match(line)
+            if match:
+                values = match
+
+            match = RE_RED_BAR.match(line)
+            if match:
+                red_bar = True
+
+            match = RE_FINISHED.match(line)
+            if match:
+                return values
+
+            match = RE_SCAN_INVALID.match(line)
+            if match:
+                if red_bar:
+                    raise RedBarError
                 else:
-                    return True
-    
-
-while args.stop_after is None or n < args.stop_after:
-    if args.use_intents:
-        p.send_intent("tesmath.calcy.ACTION_ANALYZE_SCREEN", "tesmath.calcy/.IntentReceiver", args.sleep_long)
-    else:
-        p.tap(7.40, 46.87, args.sleep_long) # Calcy IV
-
-
-    try:
-        check_calcy_logcat(p)
-        skip_count = 0
-    except pokemonlib.RedBarError:
-        print("RedBarError, continuing")
-        continue
-    except pokemonlib.CalcyIVError:
-        print("CalcyIVError")
-        if args.wait_after_error:
-            input("CalcyIVError, Press enter to continue")
-        skip_count = skip_count + 1
-        if skip_count > args.max_retries and args.max_retries != 0:
-            print("CalcyIVError " + str(args.max_retries) + " times in a row, skipping to next pokemon")
-            n = n + 1
-            p.swipe(97.22, 70.31, 4.62, 70.31, args.sleep_short)
-            skip_count = 0
-        continue
-
-    if not args.no_rename:
-        p.tap(54.91, 69.53, 0) # Dismiss Calcy IV
-        p.tap(args.name_line_x, args.name_line_y, args.sleep_short) # Rename
-        if args.nopaste:
-            p.tap(args.edit_box_x, args.edit_box_y, args.sleep_short) # Press in the edit box
-            p.swipe(args.edit_line_x, args.edit_line_y, args.edit_line_x, args.edit_line_y, args.sleep_short, 600) # Use swipe to simulate a long press to bring up copy/paste dialog
-            p.tap(args.paste_button_x, args.paste_button_y, args.sleep_short) # Press paste
-        else:
-            p.key(279, args.sleep_short) # Paste into rename
-        p.tap(args.ok_button_x, args.ok_button_y, args.sleep_short) # Press OK on edit line
-        p.tap(args.save_button_x, args.save_button_y, args.sleep_long) # Press OK on Pokemon go rename dialog
-    n = n + 1
-    p.swipe(97.22, 70.31, 4.63, 70.31, args.sleep_short) # Swipe to next pokemon
+                    raise CalcyIVError
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Pokemon go renamer')
+    parser.add_argument('--device-id', type=str, default=None,
+                        help="Optional, if not specified the phone is automatically detected. Useful only if you have multiple phones connected. Use adb devices to get a list of ids.")
+    parser.add_argument('--max-retries', type=int, default=5,
+                        help="Maximum retries, set to 0 for unlimited.")
+    parser.add_argument('--config', type=str, default="config.yaml",
+                        help="Config file location.")
+    args = parser.parse_args()
+    asyncio.run(Main(args).start())
--- pokemonlib.py
+++ pokemonlib.py
@@ -1,10 +1,8 @@
-import time
-import argparse
-import subprocess
-import logging
-import select
 from PIL import Image
 from io import BytesIO
+import asyncio
+import logging
+import subprocess
 
 logger = logging.getLogger('PokemonGo')
 logger.setLevel(logging.DEBUG)
@@ -24,150 +22,60 @@ class PhoneNotConnectedError(Exception):
     pass
 
 class PokemonGo(object):
-    def __init__(self, device_id):
-        devices = self.get_devices()
-        if devices == [] or (device_id is not None and device_id not in devices):
-            raise PhoneNotConnectedError
-        if device_id is None:
-            self.device_id = devices[0]
-        else:
-            self.device_id = device_id
-        self.use_fallback_screenshots = False
-        self.resolution = None
+    def __init__(self):
+        self.device_id = None
 
-    def run(self, args):
+    async def set_device(self, device_id=None):
+        self.device_id = device_id
+
+    async def get_device(self):
+        if self.device_id:
+            return self.device_id
+        devices = await self.get_devices()
+        if devices == []:
+            raise PhoneNotConnectedError
+        self.device_id = devices[0]
+        return self.device_id
+
+    async def run(self, args):
         logger.debug("Running %s", args)
         p = subprocess.Popen([str(arg) for arg in args], stdout=subprocess.PIPE)
         stdout, stderr = p.communicate()
         logger.debug("Return code %d", p.returncode)
         return (p.returncode, stdout, stderr)
 
-    def screencap(self):
-        if not self.use_fallback_screenshots:
-            return_code, stdout, stderr = self.run(["adb", "-s", self.device_id, "exec-out", "screencap", "-p"])
-            try:
-                return Image.open(BytesIO(stdout))
-            except (OSError, IOError):
-                logger.debug("Screenshot failed, using fallback method")
-                self.use_fallback_screenshots = True
-        return_code, stdout, stderr = self.run(["adb", "-s", self.device_id, "shell", "screencap", "-p", "/sdcard/screen.png"])
-        return_code, stdout, stderr = self.run(["adb", "-s", self.device_id, "pull", "/sdcard/screen.png", "."])
-        image = Image.open("screen.png")
-        return image
-
-    def determine_resolution(self):
-        image = self.screencap()
-        # Try and detect software nav bar
-        rgb_image = image.convert('RGB')
-        i = 1
-        bar_color = rgb_image.getpixel((0, image.size[1]-i))
-        color = bar_color
-        while color == bar_color and i < image.size[0]:
-            i = i + 1
-            color = rgb_image.getpixel((0, image.size[1]-i))
-        # If we have the same color covering 5-10% of the total height, it's probably a nav bar
-        if i > image.size[1] / 20 and i < image.size[1] / 10:
-            size = (image.size[0], image.size[1] - i)
-        else:
-            size = image.size
-        logger.info("Determined device resolution as %s", size)
-        return size
-
-    def get_resolution(self):
-        if self.resolution is None:
-            self.resolution = self.determine_resolution()
-        return self.resolution
-
-    def get_x(self, percent):
-        width, height = self.get_resolution()
-        return int((width / 100.0) * percent)
-
-    def get_y(self, percent):
-        width, height = self.get_resolution()
-        return int((height / 100.0) * percent)
-
-    def tap(self, x, y, sleep):
-        self.run(["adb", "-s", self.device_id, "shell", "input", "tap", self.get_x(x), self.get_y(y)])
-        time.sleep(sleep)
-
-    def key(self, key, sleep):
-        self.run(["adb", "-s", self.device_id, "shell", "input", "keyevent", key])
-        time.sleep(sleep)
-
-    def swipe(self, x1, y1, x2, y2, sleep, duration=None):
-        width, height = self.get_resolution()
-        args = [
-            "adb",
-            "-s",
-            self.device_id,
-            "shell",
-            "input",
-            "swipe",
-            self.get_x(x1),
-            self.get_y(y1),
-            self.get_x(x2),
-            self.get_y(y2)
-        ]
-        if duration:
-            args.append(duration)
-        self.run(args)
-        time.sleep(sleep)
-
-    def check_pixel(self, rgb_image, x, y, rgb):
-        img_rgb = rgb_image.getpixel((self.get_x(x), self.get_y(y)))
-        logger.debug("Checking pixel. %dx%d image is %s, want %s. Returning %s", self.get_x(x), self.get_y(y), img_rgb, rgb, img_rgb == rgb)
-        return img_rgb == rgb
-
-    def check_calcy_iv_img(self, rgb_image):
-        x1 = self.get_x(22.22)
-        y1 = self.get_y(82.29)
-        x2 = self.get_x(77.78)
-        y2 = self.get_y(87.50)
-        search_colors = [
-            (0xA9, 0xA9, 0xA9),
-            (0xB4, 0xB4, 0xB4),
-            (0x64, 0x64, 0x64),
-            (0x66, 0x66, 0x66)
-        ]
-        for x in range(x1, x2):
-            for y in range(y1, y2):
-                img_rgb = rgb_image.getpixel((x, y))
-                if img_rgb in search_colors:
-                    search_colors.remove(img_rgb)
-                    if search_colors == []:
-                        return True
-        return False
-        
-    def check_calcy_iv(self):
-        image = self.screencap()
-        rgb_image = image.convert('RGB')
-        if self.check_calcy_iv_img(rgb_image) is False: # Calcy IV Failed?
-            if self.check_pixel(rgb_image, 4.62, 6.77, (0xF0, 0x4B, 0x5F)) is True:
-                raise RedBarError
-            else:
-                raise CalcyIVError
-
-    def get_devices(self):
-        code, stdout, stderr = self.run(["adb", "devices"])
+    async def get_devices(self):
+        code, stdout, stderr = await self.run(["adb", "devices"])
         devices = []
         for line in stdout.decode('utf-8').splitlines()[1:-1]:
             device_id, name = line.split('\t')
             devices.append(device_id)
         return devices
 
-    def send_intent(self, intent, package, sleep):
-        return_code, stdout, stderr = self.run(["adb", "-s", self.device_id, "shell", "am broadcast -a {} -n {}".format(intent, package)])
-        time.sleep(sleep)
-
-    def read_logcat(self):
-        lines = []
-        self.logcat_task.stdout.flush()
-        while select.select([self.logcat_task.stdout], [], [], 0.1)[0] != []:
-            lines.append(self.logcat_task.stdout.readline())
-        return lines
-
-    def start_logcat(self):
-        return_code, stdout, stderr = self.run(["adb", "-s", self.device_id, "shell", "pidof", "-s", "tesmath.calcy"])
+    async def start_logcat(self):
+        return_code, stdout, stderr = await self.run(["adb", "-s", await self.get_device(), "shell", "pidof", "-s", "tesmath.calcy"])
         pid = stdout.decode('utf-8').strip()
-        self.logcat_task = subprocess.Popen(["adb", "-s", self.device_id, "logcat", "--pid={}".format(pid)], stdout=subprocess.PIPE)
-        
+        self.logcat_task = await asyncio.create_subprocess_exec(
+            *["adb", "-s", await self.get_device(), "logcat", "--pid={}".format(pid)],
+            stdout=asyncio.subprocess.PIPE
+        )
+        # Seek to the end of the file
+        while True:
+            try:
+                task = await asyncio.wait_for(self.read_logcat(), 0.1)
+            except asyncio.TimeoutError:
+                break
+
+    async def read_logcat(self):
+        line = await self.logcat_task.stdout.readline()
+        logger.debug("Received logcat line: %s", line)
+        return line
+
+    async def send_intent(self, intent, package):
+        await self.run(["adb", "-s", await self.get_device(), "shell", "am broadcast -a {} -n {}".format(intent, package)])
+
+    async def tap(self, x, y):
+        await self.run(["adb", "-s", await self.get_device(), "shell", "input", "tap", x, y])
+
+    async def key(self, key):
+        await self.run(["adb", "-s", self.device_id, "shell", "input", "keyevent", key])
